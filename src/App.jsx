8d032f4010494ccd44f12928ee47c4ee85416211
Ash Moreno <ashterfuentes@gmail.com>
2022-12-21 13:00:28 -0600

    button to generate random data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import MyComponent from "./Component";
+import Button from "./Button";
 
 function App() {
   return (
     <div className="App">
       <h1>React Task 1</h1>
       <MyComponent />
+      <Button />
     </div>
   );
 }
---
+++ src/Button.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Button() {
+  return <button className="Button">Generate Random</button>;
+}
